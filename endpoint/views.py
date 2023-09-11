a6f9e47e7af476f6ac0900e18b2b79809b530343
Awoyemi Precious <pawoyemi@gmail.com>
2023-09-11 10:21:26 +0100

    initial commit

---
+++ endpoint/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from http import HTTPStatus
+from django.http import JsonResponse
+from django.views import View
+from datetime import datetime
+import pytz
+
+
+def index(request):
+    slack_name = request.GET.get("slack_name")
+    track = request.GET.get("track")
+
+    # Get current UTC time
+    utc = datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    response = {
+        "slack_name": slack_name,
+        "current_day": datetime.now().strftime("%A"),
+        "utc_time": utc,
+        "track": track,
+        "github_file_url": "https://github.com/Bla-vk/Stage_One/blob/main/endpoint/views.py",
+        "github_repo_url": "https://github.com/Bla-vk/Stage_One",
+        "status_code": HTTPStatus.OK,
+    }
+
+    return JsonResponse(response)
